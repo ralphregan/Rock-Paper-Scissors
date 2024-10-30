85360af67c8b5fcf254824a2713880daebcef0c9
reganralph <reganralph@gmail.com>
2024-10-30 03:17:54 -0400

    Work in progress

--- TheGame.js
+++ TheGame.js
@@ -1,85 +1,168 @@
-const getComputerChoice = function(){
-    let random = Math.floor(Math.random() * 3) + 1;
-   
-    if(random === 1){
-        return "rock";
-    }
-    else if (random === 2){
-        return "paper";
-    }
-    else if (random === 3){
-        return "scissors";
-    }
-  
-
-   
-}
-const getHumanChoice = function(answer){
-    answer = prompt("Hello User, Kindly insert a number between 1 and 3!");
-    if(answer == 1){
-        return "rock";
-    }
-    else if (answer == 2){
-        return "paper";
-    }
-    else if (answer == 3){
-        return "scissors";
-    }
-    else{
-        return "nothing, Please pickout a number between 1 to 3"
-    }
-   
-
-}
-var HumanScore = 0;
-var computerScore = 0;
-
-const Play = function() {
-    for(let i = 0 ; i < 5; i++){
-        console.log( playRound(getHumanChoice(), getComputerChoice()));
-
-    } 
-    if(HumanScore > computerScore){
-        console.log("the Human won by " + HumanScore+ " to " + computerScore)
-    }   
-    else if(computerScore > HumanScore){
-        console.log("The Computer won by " + computerScore + " to " +HumanScore)
-    }
+/*
+const HumanChoice = document.querySelectorAll(".btn");
 
 
 
-}
-const playRound = function (humanChoice, computerChoice){
-    
-    if(humanChoice == "paper" && computerChoice == "rock"){
-        HumanScore = HumanScore + 1;
-        console.log("The Computer picked " + computerChoice + " and the Human picked " + HumanScore );
-        return "The Human wins this round, The Score is Human " + HumanScore + " and Computer " + computerScore;
-    }
-    else if(humanChoice == "scissors" && computerChoice == "paper"){
-        console.log("The Computer picked " + computerChoice + " and the Human picked " + HumanScore );
-        HumanScore = HumanScore + 1;
-        return "The Human wins this round, The Score is Human " + HumanScore + " and Computer " + computerScore;
-    }
-    else if(humanChoice == "rock" && computerChoice == "scissors"){
-        HumanScore = HumanScore + 1;
-        console.log("The Computer picked " + computerChoice + " and the Human picked " + HumanScore )
-        return "The Human wins this round, The Score is Human " + HumanScore + " and Computer " + computerScore;
-    }
-    else if(humanChoice == computerChoice){
-        console.log("The Computer picked " + computerChoice + " and the Human picked " + HumanScore )
-        return "The Round is a Draw, The Score is Human " + HumanScore + " and Computer " + computerScore;
-    }
-    else{
-        computerScore = computerScore + 1;
-        console.log("The Computer picked " + computerChoice + " and the Human picked " + HumanScore )
-        return "The Computer wins this round, The Score is Human " + HumanScore + " and Computer " + computerScore;
 
+HumanChoice.forEach(one => {
+    one.addEventListener("click", () =>{
+
+        const tally = 0;
+        HumanScore = 0;
+        ComScore = 0;
+        
+
+       const ComputerChoice = function(){
+    const choice = Math.floor(Math.random() * 3) + 1;
+
+    if(choice == 1){
+        return "rock"
+    }
+    else if(choice == 2){
+        return "paper"
+    }
+    else if (choice == 3){
+        return "scissors"
     }
 }
+      
 
-console.log(Play());
 
+        const GamePlay = function(ComputerChoice){
+             const value = (one.textContent).toLocaleLowerCase();
+            
+             const declareWinner = document.querySelector("h2");
+                if(value == ComputerChoice ){
+                    declareWinner.textContent = "Draw because you played " + value + "and Com played " + ComputerChoice
+                }
+                
+             else if(value == "rock" && ComputerChoice == "paper" ){
+                declareWinner.textContent = "You lost because you played " + value + "and Com played " + ComputerChoice
+                ComScore++;
 
-;
+             }
+             else if (value == "rock" && ComputerChoice == "scissors" ){
+                declareWinner.textContent = "You Won because you played " + value + "and Com played " + ComputerChoice
+                return HumanScore++;
+             }
+             else if(value == "paper" && ComputerChoice == "scissors"){
+                declareWinner.textContent = "You lost because you played " + value + "and Com played " + ComputerChoice
+               return ComScore++;
+             }
+             else if(value == "paper" && ComputerChoice == "rock"){
+                declareWinner.textContent = "You won because you played " + value + "and Com played " + ComputerChoice
+                return HumanScore++;
+             }
+             else if(value == "scissors" && ComputerChoice == "paper"){
+                declareWinner.textContent = "You won because you played " + value + "and Com played " + ComputerChoice
+                return HumanScore++;
+             }
+             else if(value == "scissors" && ComputerChoice == "rock"){
+                declareWinner.textContent = "You lost because you played " + value + "and Com played " + ComputerChoice
+                return ComScore++;
+             }
+             setTimeout(() => {declareWinner.textContent= "Want to play One More time"
 
+             },10000)
+
+        }
+
+        
+        const Round = function(){
+            for(let i = 0; i < 5; i++){
+                GamePlay(ComputerChoice());
+            }
+                if(HumanScore > ComScore){
+                    document.querySelector("h1").textContent ="You won"
+                
+                }
+                else if(HumanScore < ComScore){
+                    document.querySelector("h1").textContent ="You lost"
+                
+                }
+        
+            
+        }
+
+        Round();
+    })
+})
+
+*/
+// Define scores and round count outside the event listener for persistence
+let HumanScore = 0;
+let ComScore = 0;
+let roundCount = 0;
+
+const HumanChoice = document.querySelectorAll(".btn");
+
+HumanChoice.forEach(one => {
+    one.addEventListener("click", () => {
+
+        // Function to generate computer choice
+        const ComputerChoice = function(){
+            const choice = Math.floor(Math.random() * 3) + 1;
+            if(choice == 1) return "rock";
+            else if(choice == 2) return "paper";
+            else if(choice == 3) return "scissors";
+        }
+
+        // GamePlay function
+        const GamePlay = function(computerChoice){
+            const value = one.textContent.toLowerCase();
+            const declareWinner = document.querySelector("h2");
+            
+
+            if(value === computerChoice) {
+                declareWinner.textContent = 'Draw! You played ' + value +' and Computer played ' + computerChoice;
+            }
+             else if (value === "rock" && computerChoice === "paper") {
+                declareWinner.textContent = 'You lost! You played ' + value +' and Computer played ' + computerChoice;
+                ComScore++;
+            } else if (value === "rock" && computerChoice === "scissors") {
+                declareWinner.textContent = 'You won! You played ' + value +' and Computer played ' + computerChoice;
+                HumanScore++;
+            } else if (value === "paper" && computerChoice === "scissors") {
+                declareWinner.textContent = 'You lost! You played ' + value +' and Computer played ' + computerChoice;
+                ComScore++;
+            } else if (value === "paper" && computerChoice === "rock") {
+                declareWinner.textContent = 'You won! You played ' + value +' and Computer played ' + computerChoice;
+                HumanScore++;
+            } else if (value === "scissors" && computerChoice === "paper") {
+                declareWinner.textContent = 'You won! You played ' + value +' and Computer played ' + computerChoice;
+                HumanScore++;
+            } else if (value === "scissors" && computerChoice === "rock") {
+                declareWinner.textContent = 'You lost! You played ' + value +' and Computer played ' + computerChoice;
+                ComScore++;
+            }
+
+            // Increment round count
+            roundCount++;
+
+            // Check if 5 rounds have been played
+            if(roundCount >= 5) {
+                setTimeout(() => {
+                    const finalResult = document.querySelector("h1");
+                    if(HumanScore > ComScore) {
+                        finalResult.textContent = "Congratulations! You won the game!";
+                       
+                    } else if (HumanScore < ComScore) {
+                        finalResult.textContent = "You lost the game. Better luck next time!";
+                       
+                    } else {
+                        finalResult.textContent = "It's a draw!";
+                       
+                    }
+
+                    // Reset scores and round count for a new game
+                    HumanScore = 0;
+                    ComScore = 0;
+                    roundCount = 0;
+                }, 1000); // Delay to show the last round's result
+            }
+        }
+
+        // Run GamePlay with the computer's choice
+        GamePlay(ComputerChoice());
+    });
+});
